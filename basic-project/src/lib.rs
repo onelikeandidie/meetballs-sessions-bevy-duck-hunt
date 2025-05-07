b62073e949a882f2f30e3df2264deb60635a1827
onelikeandidie <onelikeandidie@proton.me>
2025-05-07 21:04:06 +0100

    Added: Animate ducks and dog

--- basic-project/src/lib.rs
+++ basic-project/src/lib.rs
@@ -16,12 +16,14 @@ impl Plugin for SimpleGamePlugin {
             .add_systems(
                 Update,
                 (
-                    handle_dead,
-                    handle_shoot_duck,
-                    handle_mouse_clicks,
-                    handle_dying,
-                    animate_ducks,
                     spawn_ducks,
+                    animate_ducks,
+                    move_ducks,
+                    handle_mouse_clicks,
+                    handle_shoot_duck,
+                    handle_dying,
+                    handle_dead,
+                    animate_dog,
                 )
                     .run_if(in_state(GameState::InGame)),
             );
@@ -71,13 +73,16 @@ pub enum DuckBehaviour {
     Dying,
 }
 
+#[derive(Component, Default)]
+struct Dog;
+
 pub fn setup_world(mut commands: Commands, game_assets: Res<GameAssets>) {
     // Create a 2d camera
     commands.spawn((Camera2d));
     // Duck hunt background colour is #40c0ff
     commands.insert_resource(ClearColor(Color::linear_rgb(0.251, 0.753, 1.0)));
     // Duck spawn timer
-    commands.insert_resource(SpawnTimer(Timer::from_seconds(5.0, TimerMode::Repeating)));
+    commands.insert_resource(SpawnTimer(Timer::from_seconds(1.0, TimerMode::Repeating)));
     // Duck hunt background
     commands.spawn((
         Sprite::from_atlas_image(
@@ -87,21 +92,19 @@ pub fn setup_world(mut commands: Commands, game_assets: Res<GameAssets>) {
                 index: 0,
             },
         ),
-        Transform::from_xyz(0.0, 0.0, 0.0),
+        Transform::from_xyz(0.0, 0.0, 1.0),
     ));
 
-    // Spawn a duck
     commands.spawn((
         Sprite::from_atlas_image(
-            game_assets.duck_spritesheet.clone(),
+            game_assets.dog_spritesheet.clone(),
             TextureAtlas {
-                layout: game_assets.duck_layout.clone(),
-                index: 1,
+                layout: game_assets.dog_layout.clone(),
+                index: 0,
             },
         ),
-        Transform::from_xyz(40.0, 40.0, 0.0),
-        Duck::default(),
-        AnimationTimer(Timer::from_seconds(0.5, TimerMode::Repeating)),
+        Transform::from_xyz(0.0, -20.0, 0.0),
+        Dog::default(),
     ));
 }
 
@@ -120,8 +123,8 @@ pub fn load_assets(
     let ducks_layout = TextureAtlasLayout::from_grid(UVec2::new(32, 32), 5, 1, None, None);
     let ducks_texture_atlas_layout = texture_atlas_layouts.add(ducks_layout);
 
-    let dog_texture = asset_server.load("textures/dog_spritesheet.png");
-    let dog_layout = TextureAtlasLayout::from_grid(UVec2::new(60, 46), 4, 1, None, None);
+    let dog_texture = asset_server.load("textures/dawg_spritesheet.png");
+    let dog_layout = TextureAtlasLayout::from_grid(UVec2::new(32, 32), 2, 1, None, None);
     let dog_texture_atlas_layout = texture_atlas_layouts.add(dog_layout);
 
     commands.insert_resource(GameAssets {
@@ -202,7 +205,65 @@ fn animate_ducks(
 #[derive(Resource)]
 struct SpawnTimer(Timer);
 
-fn spawn_ducks(mut commands: Commands, timer: Res<SpawnTimer>) {}
+fn spawn_ducks(
+    mut commands: Commands,
+    mut timer: ResMut<SpawnTimer>,
+    game_assets: Res<GameAssets>,
+    time: Res<Time>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.just_finished() {
+        // Spawn a duck
+        let our_sins = time.elapsed_secs().sin();
+        let x = our_sins * 120.0;
+        commands.spawn((
+            Sprite::from_atlas_image(
+                game_assets.duck_spritesheet.clone(),
+                TextureAtlas {
+                    layout: game_assets.duck_layout.clone(),
+                    index: 0,
+                },
+            ),
+            Transform::from_xyz(x, -40.0, 0.0),
+            Duck {
+                behaviour: match our_sins {
+                    -1.0..0.0 => DuckBehaviour::FlyingRight,
+                    0.0..1.0 => DuckBehaviour::FlyingLeft,
+                    _ => panic!("WHAT???"),
+                },
+                speed: (our_sins * our_sins) * 80.0 + 20.0,
+            },
+            AnimationTimer(Timer::from_seconds(0.5, TimerMode::Repeating)),
+        ));
+    }
+}
+
+fn move_ducks(time: Res<Time>, mut duck_query: Query<(&mut Transform, &mut Duck), Without<Dead>>) {
+    for (mut transform, mut duck) in duck_query {
+        if duck.behaviour == DuckBehaviour::Dying {
+            continue;
+        }
+        let x_speed = if duck.behaviour == DuckBehaviour::FlyingRight {
+            duck.speed
+        } else {
+            -duck.speed
+        };
+        transform.translation.x += x_speed * time.delta_secs();
+        transform.translation.y += duck.speed * time.delta_secs();
+        if transform.translation.x > 120.0 {
+            duck.behaviour = DuckBehaviour::FlyingLeft;
+        }
+        if transform.translation.x < -120.0 {
+            duck.behaviour = DuckBehaviour::FlyingRight;
+        }
+    }
+}
+
+fn animate_dog(time: Res<Time>, mut dog_query: Query<(&mut Transform, &mut Dog)>) {
+    for (mut transform, mut duck) in dog_query {
+        transform.translation.y = (time.elapsed_secs() * 20.0).sin() * 4.0 - 20.0;
+    }
+}
 
 #[derive(Event)]
 enum GameInputEvent {
@@ -239,7 +300,7 @@ fn handle_shoot_duck(
             // Hitbox is the 32x32 tile of the sprite
             for (entity, transform, mut duck) in &mut duck_query {
                 let pos = transform.translation.xy();
-                let hitbox = Rect::new(pos.x - 32.0, pos.y - 32.0, pos.x + 32.0, pos.y + 32.0);
+                let hitbox = Rect::new(pos.x - 16.0, pos.y - 16.0, pos.x + 16.0, pos.y + 16.0);
                 println!("{:?} {:?} {:?}", pos, hitbox, shot_pos);
                 if hitbox.contains(shot_pos.clone()) {
                     duck.behaviour = DuckBehaviour::Dying;
